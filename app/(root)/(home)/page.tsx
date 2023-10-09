04e798a18a60a376967b0b9d81929f98188e601a
Liquan Yang <yang.liquan87@gmail.com>
2023-10-08 21:03:58 -0400

    add auth

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,11 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <h1 className="text-3xl font-bold leading-10 tracking-tighter">
-          Next.js 13 we&apos;re coming!
-        </h1>
-      </div>
-    </main>
+    <div>
+      <UserButton afterSignOutUrl="/" />
+    </div>
   );
 }
